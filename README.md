1827c656ac29b86d10a8b862a9ba351d3d356af0
xmchxcoder <1394466835@qq.com>
2021-11-24 19:00:47 +0800

    重构test.c: 增加TEST_ERROR宏

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 > 一个轻量的json库
+> [Json-tutorial](https://github.com/cs-learning-every-day/json-tutorial/blob/master/tutorial02/leptjson.c)
 
 #### Run
 ```bash
--- test.c
+++ test.c
@@ -20,6 +20,14 @@ static int test_pass = 0;
 
 #define EXPECT_EQ_INT(expect, actual) EXPECT_EQ_BASE((expect) == (actual), expect, actual, "%d")
 
+#define TEST_ERROR(error, json)\
+    do {\
+        lept_value v;\
+        v.type = LEPT_FALSE;\
+        EXPECT_EQ_INT(error, lept_parse(&v, json));\
+        EXPECT_EQ_INT(LEPT_NULL, lept_get_type(&v));\
+    } while(0)
+
 static void test_parse_null() {
     lept_value v;
     v.type = LEPT_TRUE;
@@ -44,15 +52,8 @@ static void test_parse_false() {
 }
 
 static void test_parse_expect_value() {
-    lept_value v;
-
-    v.type = LEPT_FALSE;
-    EXPECT_EQ_INT(LEPT_PARSE_EXPECT_VALUE, lept_parse(&v, ""));
-    EXPECT_EQ_INT(LEPT_NULL, lept_get_type(&v));
-
-    v.type = LEPT_FALSE;
-    EXPECT_EQ_INT(LEPT_PARSE_EXPECT_VALUE, lept_parse(&v, " "));
-    EXPECT_EQ_INT(LEPT_NULL, lept_get_type(&v));
+    TEST_ERROR(LEPT_PARSE_EXPECT_VALUE, "");
+    TEST_ERROR(LEPT_PARSE_EXPECT_VALUE, " ");
 }
 
 static void test_parse_invalid_value() {
